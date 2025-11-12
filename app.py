1dbe120a6c2c45238f538d7d70fed54c3629db3a
tumelo-maja <mogale.ramsey@gmail.com>
2025-11-12 07:04:09 +0000

    shorten page names

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from app_pages.multipage import MultiPage
 
 st.set_page_config(
     page_title="Powdery Mildew Detector",
-    page_icon="🍒",
-    layout="wide"
+    page_icon="🍃"
 )
 
 # Load CSS
@@ -34,10 +33,10 @@ from app_pages.page_ml_performance import page_ml_performance_diagnostics
 app = MultiPage(app_name="Powdery Mildew Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
-app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Project Summary", page_summary_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("Leaf Visualiser", page_leaf_visualiser_body)
-app.add_page("Powdery Mildew detector", page_powdery_mildew_detector_body)
-app.add_page("ML Performance Diagnostics", page_ml_performance_diagnostics)
+app.add_page("Mildew detector", page_powdery_mildew_detector_body)
+app.add_page("ML Performance", page_ml_performance_diagnostics)
 
 app.run()  # Run the app
